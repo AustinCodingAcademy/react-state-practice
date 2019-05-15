f0a6ed9b7950756f9181e4b2032a5bc6e2817532
Jon Woo <jw56578@gmail.com>
2019-05-15 13:08:57 -0500

    readme

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import InputForm from "./InputForm";
-import * as serviceWorker from './serviceWorker';
 
 function render(){
    ReactDOM.render(<div>
