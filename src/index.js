aac058f9073af9cbd19357abee0ef281b033aaef
Jon Woo <jw56578@gmail.com>
2019-05-15 13:20:15 -0500

    readme

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import InputForm from "./InputForm";
 
 function render(){
    ReactDOM.render(<div>
-       <App />
        <div>
            Person 1
            <InputForm refresh={render}/>
